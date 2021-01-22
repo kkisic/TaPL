53a22ab20b30d3b29f41e8a6363382d208f3b546
kkisic <amareamo.pxhxc@gmail.com>
2021-01-22 18:52:48 +0900

    fix

--- src/cti.ts
+++ src/cti.ts
@@ -14,22 +14,32 @@ type FruitsMap = {
     orange : {name: "orange"}
 }
 
-type Filter<T> = T extends {price: number} ? T : never;
-type Banana = Filter<{name: "banana", price: number}>;
-type Orange = Filter<{name: "orange"}>;
+type _Filter<T> = T extends {price: number} ? T : never;
+type _Banana = _Filter<{name: "banana", price: number}>;
+type _Orange = _Filter<{name: "orange"}>;
 
-type FilteredMap<T> = {[K in keyof T]: T[K] extends Filter<T[K]> ? T[K] : never};
-type FilteredFruits = FilteredMap<FruitsMap>;
+type _FilteredMap<T> = {[K in keyof T]: T[K] extends _Filter<T[K]> ? T[K] : never};
+type _FilteredFruits = _FilteredMap<FruitsMap>;
 
-type NonNeverKeys<T> = {[K in keyof T]: T[K] extends never ? never : K}[keyof T];
-type NonNeverKeys_<T> = {[K in keyof T]: T[K] extends never ? never : K};
-type NonNeverFruitsKeys = NonNeverKeys<FilteredFruits>;
-type NonNeverFruitsKeys_ = NonNeverKeys_<FilteredFruits>;
+type _NonNeverKeys<T> = {[K in keyof T]: T[K] extends never ? never : K}[keyof T];
+type _NonNeverKeys_<T> = {[K in keyof T]: T[K] extends never ? never : K};
+type _NonNeverFruitsKeys = _NonNeverKeys<_FilteredFruits>;
+type _NonNeverFruitsKeys_ = _NonNeverKeys_<_FilteredFruits>;
 
 type hoge = {a: "aa", b: "bb", c: "cc", d: never};
 type fuga = hoge[keyof hoge];
 type foo = {[K in keyof hoge]: hoge[K] extends never ? never : K}[keyof hoge];
-type Foo = {[K in keyof FilteredFruits]: FilteredFruits[K] extends never ? never : K}[keyof FilteredFruits];
+type Foo = {[K in keyof _FilteredFruits]: _FilteredFruits[K] extends never ? never : K}[keyof _FilteredFruits];
 type Bar = {[K in keyof FruitsMap]: FruitsMap[K] extends never ? never : K}[keyof FruitsMap];
 
-type PickedFruits = Pick<FruitsMap, NonNeverFruitsKeys>
+type _PickedFruits = Pick<FruitsMap, _NonNeverFruitsKeys>
+
+type Filter<T, U> = T extends U ? T : never;
+type FilteredMap<T, U> = {[K in keyof T]: T[K] extends Filter<T[K], U> ? T[K] : never};
+type NonNeverKeys<T> = {[K in keyof T]: T[K] extends never ? never : K}[keyof T];
+type PickUp<T, U> = Pick<T, NonNeverKeys<FilteredMap<T, U>>>;
+
+type FilteredFruits = FilteredMap<FruitsMap, {price: number}>;
+type NonNeverFruitsKeys = NonNeverKeys<FilteredFruits>;
+type PickedFruits_ = Pick<FruitsMap, NonNeverFruitsKeys>;
+type PickedFruits = PickUp<FruitsMap, {price: number}>;
--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ type Zero = "0";
 type Num = Zero | {pred: Num};
 
 type Succ<T extends Num> = {pred: T};
-type Pred<T extends Num> = T extends Succ<T> ? T : Zero;
+type Pred<T extends Num> = T extends Succ<Num> ? T["pred"] : Zero;
 type IsZero<T extends Num> = T extends Zero ? True : False;
 
 let c : Succ<Zero>;
@@ -19,3 +19,21 @@ let d : Succ<typeof c>;
 let e : Pred<typeof c>;
 let f : Pred<Zero>;
 let g : Pred<Succ<Zero>>;
+let h : Pred<Succ<Succ<Zero>>>;
+let i : Pred<Succ<Pred<Zero>>>;
+
+//type instantiation is excessively deep and possibly infinite
+//type Add<T extends Num, U extends Num> = U extends Zero ? T : Add<Succ<T>, Pred<U>>;
+type Add<T extends Num, U extends Num> = {true: T, false: Succ<Add<T, Pred<U>>>}[IsZero<U>];
+type Sub<T extends Num, U extends Num> = T extends Zero ? T : U extends Zero ? T : Sub<Pred<T>, Pred<U>>;
+//type Sub<T extends Num, U extends Num> = {true: T, false: {true: T, false: Sub<Pred<T>, Pred<U>>}[IsZero<U>]}[IsZero<T>];
+
+let add1 : Add<Succ<Zero>, Succ<Succ<Succ<Zero>>>>;
+let sub1 : Sub<Zero, Zero>
+let sub2 : Sub<Succ<Zero>, Succ<Zero>>;
+let sub3 : Sub<Succ<Succ<Succ<Zero>>>, Succ<Zero>>;
+
+type Mul<T extends Num, U extends Num> = U extends Zero ? Zero : Add<T, Mul<T, Pred<U>>>;
+//type Mul<T extends Num, U extends Num> = {true: Zero, false: Add<T, Mul<T, Pred<U>>>}[IsZero<U>];
+let mul1 : Mul<Succ<Zero>, Zero>;
+let mul2 : Mul<Succ<Succ<Zero>>, Succ<Succ<Succ<Zero>>>>;
